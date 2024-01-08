b9902e5dc4281e0cf547cfebdbc6b7d7a9c4312a
Rique <aguirree1985@gmail.com>
2024-01-08 13:58:58 -0800

    Adds validations for create and update

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,6 @@
 class Recipe < ApplicationRecord
     belongs_to :user
+    validates :title, :ingredients, :instructions, :image, presence: true
+    validates :ingredients, :instructions, :image, length: { minimum:10 }
+    validates :title, length: { minimum:5 }
 end
